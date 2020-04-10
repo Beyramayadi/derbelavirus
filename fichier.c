731b2a58673be39a051efc66acddd35ed76d139d
yousseflouhaichi <61736874+yousseflouhaichi@users.noreply.github.com>
2020-04-10 10:06:57 +0100

    Create fichier.c

---
+++ fichier.c
@@ -0,0 +1,28 @@
+void initialiser_ennemi(ent_sec *ennemi){
+
+ennemi->vie=100;
+
+position_actuelle=0;
+position_deces=0;
+
+ennemi->normal[0]= IMG_Load("");
+ennemi->normal[1]= IMG_Load("");
+ennemi->normal[2]= IMG_Load("");
+ennemi->normal[3]= IMG_Load("");
+ennemi->normal[4]= IMG_Load("");
+
+
+ennemi->deces[0]= IMG_Load("");
+ennemi->deces[1]= IMG_Load("");
+ennemi->deces[2]= IMG_Load("");
+ennemi->deces[3]= IMG_Load("");
+ennemi->deces[4]= IMG_Load("");
+
+
+ennemi->position.x = 0;
+ennemi->position.y = 800;
+ennemi->position.w = ennemi->normal[0]->w;
+ennemi->position.h = ennemi->normal[0]->h;
+
+
+}
