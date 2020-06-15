8325b7ca903fdbb83233bf3da910f0b434aa0cf8
biloute <57026220+oui-oui123@users.noreply.github.com>
2020-06-14 18:17:52 -0700

    Add files via upload

---
+++ Beyram Ayadi SHarescreen/mainshare.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "screenshare.h"
+
+
+int main()
+{
+
+    SDL_Surface*ecran=NULL;
+    background back;
+    back=Initialisation_map(back);
+    SDL_Event e;
+    int c=1;
+    SDL_Init(SDL_INIT_EVERYTHING);
+    ecran = SDL_SetVideoMode(1200,640, 32, SDL_HWSURFACE);
+    while(c)
+    {
+        affichage_backi(back,ecran);
+        back=scroll1(back,e);
+        back=scroll2(back,e);
+        //affichage_background(back,ecran);
+        SDL_PollEvent(&e);
+        switch(e.type)
+        {
+            case SDL_QUIT:
+            c=0;
+            break;
+        }
+
+
+    }
+    SDL_Quit();
+
+
+
+
+return 0;
+}
