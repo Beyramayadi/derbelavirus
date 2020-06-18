499e5751e9409b5f03f3c0114bb104ffa1d04961
biloute <57026220+oui-oui123@users.noreply.github.com>
2020-06-18 04:08:07 -0700

    Add files via upload

---
+++ Beyram Ayadi SHarescreen/html/search/searchdata.js
@@ -0,0 +1,18 @@
+var indexSectionsWithContent =
+{
+  0: "b",
+  1: "b"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes"
+};
+
+var indexSectionLabels =
+{
+  0: "Tout",
+  1: "Classes"
+};
+
